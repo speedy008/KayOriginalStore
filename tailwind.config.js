fe89d56fce7404dcf38638e25ba315c66c8008d7
ksolankicorra <ksolanki@corra.com>
2024-10-14 16:22:51 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layouts/*.liquid',
+    './sections/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './snippets/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
